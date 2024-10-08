0923a22b9947890a8569cd6f9048877d85a14965
Alecaboo <alec.malcangio@icloud.com>
2024-10-07 23:51:06 -0600

    First sensor test sketch
    Super simple Arduino code to light an LED with a signal from hall effect sensor.

---
+++ hallEffectTestSketch.ino
@@ -0,0 +1,20 @@
+int magnetPin = 3;
+int ledPin = 4;
+
+
+void setup() {
+  Serial.begin(5600);
+  // There's an argument to use the internal pullup resistor here, but I honestly need to futz with pullup stuff more.
+  // also an argument to just physically build a noise filter, because I don't really trust this that much.
+  pinMode(magnetPin,INPUT);
+  pinMode(ledPin,OUTPUT);
+
+}
+
+void loop() {
+  // turns on an LED if there's a signal from the hall effect sensor. Look, it's nothing crazy, but we gotta do tests
+  
+  digitalWrite(ledPin,digitalRead(magnetPin));
+
+
+}
